e50a0fba2c0154639b965d26910f4ba884c5b43e
KMinh <daukhacminh006@gmail.com>
2023-05-24 17:16:46 +0700

    Build fix

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <main className='content'>
         <img src='https://assets.editorial.aetnd.com/uploads/2018/08/outbreak-of-world-war-i-gettyimages-506127736.jpg' style={{ width: '600px', height: 'auto'}} />
         <h2>What was the turning point that caused the assassination of Archduke Franz Ferdinand?</h2>
-        <p style={{ margin: '0 20%', textAlign: 'center'}}>While the assassination of Archduke Franz Ferdinand is often attributed to the Black Hands skill, the true turning point leading to the event can be traced back to a series of mishaps and luck throughout the assassination plan that were able to somehow lead to a perfect moment for the Archduke's death.</p>
+        <p style={{ margin: '0 20%', textAlign: 'center'}}>While the assassination of Archduke Franz Ferdinand is often attributed to the Black Hands skill, the true turning point leading to the event can be traced back to a series of mishaps and luck throughout the assassination plan that were able to somehow lead to a perfect moment for the Archduke&apos;s death.</p>
       </main>
     </div>
   )
