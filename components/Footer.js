24bee6b959c3b985c9c5eaf98c1d4f0550dfb6f0
KMinh <daukhacminh006@gmail.com>
2023-05-24 22:27:42 +0700

    Touch ups

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='Footer'>
-      <p>Khac Minh Dau, Daniel Hurley, Tony Nguyen</p>
+      <p>Khac Minh Dau, Danny Hurley, Tony Nguyen</p>
     </div>
   )
 }
